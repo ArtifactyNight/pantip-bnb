265b7186625f1cec554cfa8748a6778e1ffe18f5
ArtifactyNight <itsnight134679@gmail.com>
2024-07-29 00:23:56 +0700

    fix: footer alignment not correctly

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,17 @@
 import { AppConfig } from "@/utils/AppConfig";
 import type React from "react";
+import { ContainerWrapper } from "./container-wrapper";
 
 export const Footer: React.FC = () => {
   return (
     <footer className="bg-white text-center">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+      <ContainerWrapper className="mx-auto px-4 py-8 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="text-sm text-muted-foreground">
+          <div className="text-sm text-muted-foreground w-full">
             © {new Date().getFullYear()} {AppConfig.name}. All rights reserved.
           </div>
         </div>
-      </div>
+      </ContainerWrapper>
     </footer>
   );
 };
